f828c1ff4f17cca9575bfb0c0cf057a5ff99fff9
Basit Anwer <anwer.basit@gmail.com>
2022-12-27 23:00:18 +0500

    Added github octo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import "../styles/globals.css";
 import "../styles/prismjs-onedark.css";
+import GithubCorner from "react-github-corner";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log(Component.name);
+  return (
+    <div>
+      <div className="absolute z-0">
+        <Component {...pageProps} />
+      </div>
+      {Component.name === "Home" ? (
+        <div className="z-20">
+          <GithubCorner
+            direction="left"
+            href="https://github.com/basitanwer/blog-v1"
+            bannerColor="#0ca5e9"
+            octoColor="white"
+          />
+        </div>
+      ) : (
+        <div className="z-20">
+          <GithubCorner
+            direction="left"
+            href="https://github.com/basitanwer/blog-v1"
+            bannerColor="white"
+            octoColor="#0ca5e9"
+          />
+        </div>
+      )}
+    </div>
+  );
 }
