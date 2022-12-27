b571e8baf6142aa9c8a44cfe1221529c587903d8
Basit Anwer <anwer.basit@gmail.com>
2022-12-27 12:04:12 +0500

    updated gql temp code

--- pages/gql.tsx
+++ pages/gql.tsx
@@ -17,10 +17,12 @@ require("prismjs/components/prism-javascript");
 require("prismjs/components/prism-css");
 
 require("prismjs/components/prism-jsx");
+
 // import "prismjs/themes/prism-twilight.css";
 import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-json";
 import "prismjs/components/prism-bash";
+import { RoughNotation } from "react-rough-notation";
 
 export async function getStaticProps({ params }: Params) {
   let fields = ["title", "date", "slug", "author", "content", "ogImage", "coverImage"];
@@ -54,6 +56,7 @@ interface Props {
   contentHtml: string;
   data: unknown;
   coverImage: string;
+  title: string;
 }
 
 export default function Gql(props: Props) {
@@ -71,12 +74,28 @@ export default function Gql(props: Props) {
       <div className="relative  h-[50vh] max-h-96 bg-sky-500 text-white text-5xl flexitems-center justify-center">
         <Image src={md} alt="cover image" className="object-cover bg-cover" fill />
       </div>
+
       <div
         id="main-box"
-        className="relative flex flex-col space-y-10 items-center justify-center shadow-2xl shadow-slate-700/70 border-spacing-2 rounded-lg mb-8  sm:mx-4 -mt-20  bg-white pt-20 p-4"
+        className="relative flex flex-col space-y-10 md:items-center justify-center shadow-2xl shadow-slate-700/70 border-spacing-2 rounded-lg mb-8  sm:mx-4 -mt-20  bg-white pt-20 p-4"
       >
+        <div className="max-w-3xl ">
+          <RoughNotation
+            strokeWidth={6}
+            type="highlight"
+            color="#fff36c"
+            animationDelay={1000}
+            iterations={1}
+            show={true}
+            multiline={true}
+          >
+            <div className="relative text-5xl text-gray-500 font-bold max-w-3xl ">
+              {props.title}
+            </div>
+          </RoughNotation>
+        </div>
         <article
-          className="prose prose-slate prose-img:rounded-lg prose-code:font-sm"
+          className="prose prose-slate prose-img:rounded-lg  max-w-3xl"
           dangerouslySetInnerHTML={{ __html: props.contentHtml }}
         ></article>
       </div>
