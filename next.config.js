97a91784e185e2c10dbc91f8ee76d42b37fc5b1a
Basit Anwer <anwer.basit@gmail.com>
2023-01-03 21:06:47 +0500

    redirects

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/apollgraphql-server-a-complete-tutorial-8e49e44f3b52",
+        destination: "/blog/apollgraphql-server-a-complete-tutorial-8e49e44f3b52",
+        permanent: true,
+      },
+      {
+        source: "/simple-task-schedular",
+        destination: "/blog/simple-task-schedular",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
